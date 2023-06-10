4fd180c929ea925cd350a99ac2bef42b058ecd4f
DevaWinner <winneraaniekan@gmail.com>
2023-06-10 10:35:14 +0100

    Add add task function and export

--- src/todo.js
+++ src/todo.js
@@ -2,3 +2,19 @@ export const loadTasksFromLocalStorage = () => {
   const tasks = localStorage.getItem('tasks');
   return tasks ? JSON.parse(tasks) : [];
 };
+
+export const saveTasksToLocalStorage = (tasks) => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+export const addTask = (description) => {
+  const tasks = loadTasksFromLocalStorage();
+  const index = tasks.length + 1;
+  const newTask = {
+    description,
+    completed: false,
+    index,
+  };
+  tasks.push(newTask);
+  saveTasksToLocalStorage(tasks);
+};
