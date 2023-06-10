78872d233c47cb5a21b2387863d19e7111525cdb
DevaWinner <winneraaniekan@gmail.com>
2023-06-10 11:00:17 +0100

    Reactor Render function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 import './style.css';
 import {
   addTask, deleteTask, editTask, saveTasksToLocalStorage, loadTasksFromLocalStorage,
-} from './todo';
+} from './todo.js';
 
 const todoList = document.getElementById('todo-list');
 const taskInput = document.getElementById('task-input');
 const addTaskButton = document.getElementById('add-task-button');
 const clearButton = document.getElementById('clear-button');
+
+const renderTasks = () => {
+  todoList.innerHTML = '';
+
+  const tasks = loadTasksFromLocalStorage();
+
+  tasks.forEach((task, index) => {
+    const listItem = document.createElement('li');
+    listItem.className = 'task-item';
+    listItem.dataset.index = index;
+
+    const taskWrapper = document.createElement('div');
+    taskWrapper.className = 'task-wrapper';
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'checkbox';
+    checkbox.checked = task.completed;
+    taskWrapper.appendChild(checkbox);
+
+    const taskText = document.createElement('p');
+    taskText.innerText = task.description;
+    taskText.className = 'task-text';
+    if (task.completed) {
+      taskText.classList.add('completed');
+    }
+    taskWrapper.appendChild(taskText);
+
+    const kebabMenu = document.createElement('i');
+    kebabMenu.className = 'bx bx-dots-vertical-rounded';
+    taskWrapper.appendChild(kebabMenu);
+
+    const deleteButton = document.createElement('i');
+    deleteButton.className = 'bx bx-trash delete-button';
+    taskWrapper.appendChild(deleteButton);
+
+    listItem.appendChild(taskWrapper);
+    todoList.appendChild(listItem);
+  });
+
+  saveTasksToLocalStorage(tasks);
+};
 
 addTaskButton.addEventListener('click', () => {
   const description = taskInput.value.trim();
@@ -64,47 +106,5 @@ todoList.addEventListener('blur', (event) => {
     renderTasks();
   }
 }, true);
-
-const renderTasks = () => {
-  todoList.innerHTML = '';
-
-  const tasks = loadTasksFromLocalStorage();
-
-  tasks.forEach((task, index) => {
-    const listItem = document.createElement('li');
-    listItem.className = 'task-item';
-    listItem.dataset.index = index;
-
-    const taskWrapper = document.createElement('div');
-    taskWrapper.className = 'task-wrapper';
-
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.className = 'checkbox';
-    checkbox.checked = task.completed;
-    taskWrapper.appendChild(checkbox);
-
-    const taskText = document.createElement('p');
-    taskText.innerText = task.description;
-    taskText.className = 'task-text';
-    if (task.completed) {
-      taskText.classList.add('completed');
-    }
-    taskWrapper.appendChild(taskText);
-
-    const kebabMenu = document.createElement('i');
-    kebabMenu.className = 'bx bx-dots-vertical-rounded';
-    taskWrapper.appendChild(kebabMenu);
-
-    const deleteButton = document.createElement('i');
-    deleteButton.className = 'bx bx-trash delete-button';
-    taskWrapper.appendChild(deleteButton);
-
-    listItem.appendChild(taskWrapper);
-    todoList.appendChild(listItem);
-  });
-
-  saveTasksToLocalStorage(tasks);
-};
 
 document.addEventListener('DOMContentLoaded', renderTasks);
