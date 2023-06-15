954a4a36cd7933a37b3eb9e3be08cec9c2dd708f
DevaWinner <winneraaniekan@gmail.com>
2023-06-15 14:59:46 +0100

    Import functions and mock local storage

--- test/todo.test.js
+++ test/todo.test.js
@@ -1,1 +1,22 @@
+import { addTask, deleteTask } from '../src/todo.js';
+import { JSDOM } from 'jsdom';
 
+// First, I will Mock localStorage
+const localStorageMock = (() => {
+  let store = {};
+
+  return {
+    getItem: jest.fn((key) => store[key] || null),
+    setItem: jest.fn((key, value) => {
+      store[key] = value.toString();
+    }),
+    removeItem: jest.fn((key) => {
+      delete store[key];
+    }),
+    clear: jest.fn(() => {
+      store = {};
+    }),
+  };
+})();
+
+Object.defineProperty(global, 'localStorage', { value: localStorageMock });
