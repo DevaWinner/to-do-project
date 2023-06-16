cc5f29290a59fbe1c499d8f7c30057957376981f
DevaWinner <winneraaniekan@gmail.com>
2023-06-16 17:36:17 +0100

    Mock Local Storage

--- test/edit-clear.test.js
+++ test/edit-clear.test.js
@@ -2,3 +2,23 @@ import { JSDOM } from 'jsdom';
 import { editTask } from '../src/todo.js';
 import { updateTaskStatus } from '../src/status.js';
 import { clearCompletedTasks } from '../src/clear.js';
+
+// First, I will Mock localStorage
+const localStorageMock = (() => {
+  let store = {};
+
+  return {
+    getItem: jest.fn((key) => store[key] || null),
+    setItem: jest.fn((key, value) => {
+      store[key] = value.toString();
+    }),
+    removeItem: jest.fn((key) => {
+      delete store[key];
+    }),
+    clear: jest.fn(() => {
+      store = {};
+    }),
+  };
+})();
+
+Object.defineProperty(global, 'localStorage', { value: localStorageMock });
