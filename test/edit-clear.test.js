cf6481fb928bdd2a2bd16626f4869b770b07f299
DevaWinner <winneraaniekan@gmail.com>
2023-06-16 17:35:56 +0100

    Import needed functions

---
+++ test/edit-clear.test.js
@@ -0,0 +1,4 @@
+import { JSDOM } from 'jsdom';
+import { editTask } from '../src/todo.js';
+import { updateTaskStatus } from '../src/status.js';
+import { clearCompletedTasks } from '../src/clear.js';
